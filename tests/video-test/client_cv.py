3e91e3f5041101469b5a78e3cdf27fa302975772
Justin Kleiber <jkleiber8@gmail.com>
2020-07-09 22:27:11 -0700

    Makes initial video frame test work

--- tests/video-test/client_cv.py
+++ tests/video-test/client_cv.py
@@ -1,15 +1,17 @@
 import cv2
-import numpy as np
 import socket
-import sys
 import pickle
 import struct ### new code
 
 cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+clientsocket.connect(('kleiber.xyz',6000))
 
 while True:
-    ret,frame=cap.read()
+    ret,frame = cap.read()
     data = pickle.dumps(frame) ### new code
-    clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
+
+    # Send message length first
+    message_size = struct.pack("L", len(data))
+
+    clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
