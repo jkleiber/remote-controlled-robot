bf5a5100cb1cc2d10b031a2bf3079942298ad3ba
Justin Kleiber <jkleiber8@gmail.com>
2020-07-23 08:15:43 +0000

    Compiles and runs the YDLidar on the raspberry pi.
    Need to buy something with more RAM

--- robot/build_ws.sh
+++ robot/build_ws.sh
@@ -2,4 +2,4 @@
 
 source docker_config.sh
 
-docker exec -it $CONTAINER_NAME bash -c "cd ros_workspace;rm -rf build;source /opt/ros/noetic/setup.bash;catkin_make;. devel/setup.bash"
+docker exec -it $CONTAINER_NAME bash -c "cd ros_workspace;rm -rf build;source /opt/ros/noetic/setup.bash;catkin_make -j1;. devel/setup.bash"
